56fe05b3677ce85ea407f9a32a9a2ef0aeee3659
Raja Sekhar Modu <54770103+rajamodu@users.noreply.github.com>
2020-04-19 14:26:24 +0200

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/rajamodu/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('rajamodu')
+            node / gitConfigEmail('modurajasekhar@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('rajamodu/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
